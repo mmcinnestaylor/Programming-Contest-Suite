d3e19f909f2b89fdceb27e451e6f7c88f20c3c76
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-07-29 02:29:30 -0400

    registering core app + set static path

--- src/contestsuite/settings.py
+++ src/contestsuite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
     'crispy_forms',
 ]
 
@@ -117,5 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
