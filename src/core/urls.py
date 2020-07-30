4573dd2ce70773edcdfec18d9e96fbfa348fa89e
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-07-30 12:39:50 -0400

    added routing for about, contact, faq, teams

---
+++ src/core/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # contest suite homepage
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('faq/', views.faq, name='faq'),
+    path('teams/', views.faq, name='teams'),
+]
--- src/core/views.py
+++ src/core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'core/index.html')
+
+
+def about(request):
+    return render(request, 'core/about.html')
+
+
+def contact(request):
+    return render(request, 'core/contact.html')
+
+
+def faq(request):
+    return render(request, 'core/faq.html')
+
+
+def teams(request):
+    return render(request, 'core/teams.html')
