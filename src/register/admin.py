926172ed8bbc428faa813e0c808764bef036e6ef
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-08-09 03:31:46 +0100

    added Register models to admin site

--- src/register/admin.py
+++ src/register/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+
+admin.site.register(models.Course)
+admin.site.register(models.Faculty)
+admin.site.register(models.Profile)
+admin.site.register(models.Team)
