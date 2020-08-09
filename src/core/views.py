938cbb13887a3ecc03d621e8f405f01d6b245783
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-08-09 04:35:11 +0100

    updated teams view to display contestant info

--- src/core/views.py
+++ src/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from register.models import Team, Profile
 
 # Create your views here.
 
@@ -20,4 +21,27 @@ def faq(request):
 
 
 def teams(request):
-    return render(request, 'core/teams.html')
+    context = {}
+
+    teams_set = Team.objects.all()
+    participants_set = Profile.objects.all()
+
+    upper_teams_set = teams_set.filter(division=2)
+    context['upper_teams'] = upper_teams_set
+
+    num_upper_teams = upper_teams_set.count()
+    context['num_upper_teams'] = num_upper_teams
+
+    num_upper_participants = participants_set.filter(team__division=2).count()
+    context['num_upper_participants'] = num_upper_participants
+
+    lower_teams_set = teams_set.filter(division=1)
+    context['lower_teams'] = lower_teams_set
+
+    num_lower_teams = lower_teams_set.count()
+    context['num_lower_teams'] = num_lower_teams
+
+    num_lower_participants = participants_set.filter(team__division=1).count()
+    context['num_lower_participants'] = num_lower_participants
+
+    return render(request, 'core/teams.html', context)
