8952545825b5a8ba826a697ea10c18888d11730b
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-08-12 21:46:34 +0100

    email backend

--- src/contestsuite/settings.py
+++ src/contestsuite/settings.py
@@ -146,9 +146,12 @@ LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Email settings
-'''EMAIL_HOST = ''
+'''
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  
+EMAIL_HOST = ''
 EMAIL_PORT = 
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'ACM Dev <webmaster@fsu.acm.org>' '''
+DEFAULT_FROM_EMAIL = 'ACM Dev <webmaster@fsu.acm.org>' 
+'''
