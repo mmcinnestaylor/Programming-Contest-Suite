9fd0320998dd3e664c0ba69a473f77f07f79cee7
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-08-17 11:07:24 +0100

    app routing

--- src/manager/urls.py
+++ src/manager/urls.py
@@ -1,1 +1,9 @@
 from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # contest suite homepage
+    path('', views.base, name='manage_base'),
+    path('profile/', views.profile, name='manage_profile'),
+]
