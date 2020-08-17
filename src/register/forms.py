4f73719e0073f7ee888e72774a833a37f8d83f59
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-08-17 20:56:15 +0100

    added team registration form

--- src/register/forms.py
+++ src/register/forms.py
@@ -7,7 +7,6 @@ from .models import Team
 # Extend built-in User form to include email, first name, and last name fields
 class ExtendedUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-
     # name lengths as specified by Django 3.0.* documentation
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=150, required=True)
@@ -16,6 +15,16 @@ class ExtendedUserCreationForm(UserCreationForm):
         model = User
         fields = ['first_name', 'last_name', 'email', 'username', 'password1', 'password2']
 
-class TeamForm(form.ModelForm)
+class TeamForm(forms.ModelForm):
     class Meta:
         model = Team
+        fields = ['name', 'division']
+        help_texts = {
+            'name': '30 characters max. Keep it PG-13 please!',
+            'division': 'The division in which your team will compete.',
+        }
+        error_messages = {
+            'name': {
+                'max_length': "This team name is too long.",
+            },
+        }
