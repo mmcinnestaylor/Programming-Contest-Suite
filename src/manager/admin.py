39a053086b421284971a0ffaa16fe874ab9b1a1e
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-08-21 05:44:33 +0100

    register faculty import-export in admin

--- src/manager/admin.py
+++ src/manager/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from . import models
 
 # Register your models here.
 
+class FacultyResource(resources.ModelResource):
+	class Meta:
+		model = models.Faculty
+		import_id_fields = ('email',)
+
+
+class FacultyAdmin(ImportExportModelAdmin):
+    resource_class = FacultyResource
+
+
+class CourseResource(resources.ModelResource):
+	class Meta:
+		model = models.Course
+		#import_id_fields = ('email',)
+
 admin.site.register(models.Course)
-admin.site.register(models.Faculty)
 admin.site.register(models.Profile)
+admin.site.register(models.Faculty, FacultyAdmin)
