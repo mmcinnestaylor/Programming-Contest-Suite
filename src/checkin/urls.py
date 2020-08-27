bca943cc0a248bef940e5497a8fee8f91761b4b7
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-08-27 03:57:16 -0400

    checkin and checkin result routing

---
+++ src/checkin/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.checkin, name='checkin'),
+    path('result/', views.checkin_result, name='checkin_result'),
+]
