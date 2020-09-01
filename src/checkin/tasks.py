c8a9fd73c380ed7a62bfa98ad67d48e3438bbfa8
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-09-01 15:59:29 -0400

    send_credentials celery task
    emails contestant the team's id and password after the contestant checks in

---
+++ src/checkin/tasks.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, unicode_literals
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from celery import shared_task
+
+
+@shared_task
+def send_credentials(username):
+    user = User.objects.get(username=username)
+    email = str(user.email)
+    team_id = user.profile.team.contest_id
+    team_password = user.profile.team.contest_password
+
+    message = 'Hello ' + str(user.first_name) + ',\nWelcome to this semester\'s ACM Programming Contest! Your DOMJudge credentials are below. Use these credentials when your team logs in to http://domjudge.cs.fsu.edu\nusername: ' + str(team_id) + '\npassword: ' + str(team_password)
+
+    send_mail(
+    'Your DOMJudge Credentials',
+    message,
+    'ACM Programming Contest <contest@fsu.acm.org>',
+    [email],
+    fail_silently=False,
+)
