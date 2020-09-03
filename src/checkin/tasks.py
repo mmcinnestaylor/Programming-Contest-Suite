024e2597a17ccb874dc5892dea95eb86ebbc4c7e
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-09-03 02:30:22 -0400

    updated task to use built in send mail method

--- src/checkin/tasks.py
+++ src/checkin/tasks.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 from celery import shared_task
 from celery.utils.log import get_task_logger
@@ -11,12 +11,9 @@ logger = get_task_logger(__name__)
 @shared_task
 def send_credentials(username):
     user = User.objects.get(username=username)
-    user_email = str(user.email)
-    team_id = user.profile.team.contest_id
-    team_password = user.profile.team.contest_password
-
-    message = 'Hello ' + str(user.first_name) + ',\nWelcome to this semester\'s ACM Programming Contest! Your DOMJudge credentials are below. Use these credentials when your team logs in to http://domjudge.cs.fsu.edu\nusername: ' + str(team_id) + '\npassword: ' + str(team_password)
-
-    send_mail('Your DOMJudge Credentials', message, 'ACM Programming Contest <contest@fsu.acm.org>', [user_email], fail_silently=False)
+    
+    subject = 'Your DOMJudge Credentials'
+    message = render_to_string('checkin/team_credentials_email.html', {'user': user})
+    user.email_user(subject, message)
 
     logger.info('Sent credentials to %s' % username)
