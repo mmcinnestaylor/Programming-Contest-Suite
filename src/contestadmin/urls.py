30d30eb2a008fba648e299a39ad734f07a69194c
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-10-07 15:15:51 -0400

    corrected paths

--- src/contestadmin/urls.py
+++ src/contestadmin/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ec_files/', views.download_ec_files, name='ec_files'),
+    path('', views.dashboard, name='admin_dashboard'),
+    path('ec_files/', views.download_ec_files, name='all_ec_files'),
     path('ec_files/<uidb64>/', views.FacExtraCreditFiles.as_view(), name='fac_ec_files'),
 ]
