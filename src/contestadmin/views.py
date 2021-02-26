a5d3dc13596a824aff9988acc749058520aeb7d6
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2021-02-26 15:15:14 -0500

    created views for all admin tasks

--- src/contestadmin/views.py
+++ src/contestadmin/views.py
@@ -3,30 +3,103 @@ import os
 from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.models import User
+from django.db import transaction
 from django.http import HttpResponse
 from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
 from django.shortcuts import redirect, render
+from django.views import View
 
 from io import BytesIO
 from zipfile import ZipFile
 
 from . import forms
 from . import tasks
+from contestadmin.models import Contest
 from contestsuite.settings import MEDIA_ROOT
-from manager.models import Course, Profile
+from manager.models import Course, Faculty, Profile
 from register.models import Team
 
 # Create your views here.
 
+class DownloadExtraCreditFiles(View):
 
-class FacExtraCreditFiles(View):
+    def get(self, request):
+        in_memory = BytesIO()
+        zip = ZipFile(in_memory, "a")
+        
+        fpath = MEDIA_ROOT + '/ec_files/'
+        for fname in os.listdir(fpath):
+            zip.write(fpath+fname, fname)
 
-    def serve(self, request, uidb64):
+        # fix for Linux zip files read in Windows
+        for file in zip.filelist:
+            file.create_system = 0 
+
+        zip.close()
+
+        response = HttpResponse(content_type="application/zip")
+        response['Content-Disposition'] = "attachment; filename=all_ec_files.zip"
+        
+        in_memory.seek(0)    
+        response.write(in_memory.read())
+        
+        return response
+
+class DownloadTSVFiles(View):
+
+    def get(self, request):
+        in_memory = BytesIO()
+        zip = ZipFile(in_memory, "a")
+        
+        fpath = MEDIA_ROOT + '/contest_files/'
+        for fname in os.listdir(fpath):
+            zip.write(fpath+fname, fname)
+
+        # fix for Linux zip files read in Windows
+        for file in zip.filelist:
+            file.create_system = 0 
+
+        zip.close()
+
+        response = HttpResponse(content_type="application/zip")
+        response['Content-Disposition'] = "attachment; filename=dj_tsv_files.zip"
+        
+        in_memory.seek(0)    
+        response.write(in_memory.read())
+        
+        return response
+
+
+class EmailFaculty(View):
+
+    def get(self, request):
+        tasks.email_faculty.delay(request.META['HTTP_HOST'])
+        messages.info(request, 'Email faculty task scheduled.', fail_silently=True)
+
+        return redirect('admin_dashboard')
+
+
+class FacultyDashboard(View):
+
+    def get(self, request, uidb64):
+        try:
+            faculty_member = Faculty.objects.get(email__contains=force_text(urlsafe_base64_decode(uidb64)))
+        except: #(TypeError, ValueError, OverflowError):
+            faculty_member = None
+
+        if faculty_member is not None:
+            context = {}
+            context['first_name'] = faculty_member.first_name
+            context['last_name'] = faculty_member.last_name
+            context['uid'] = uidb64
+
+            return render(request,'contestadmin/faculty_dashboard.html', context)
+    
+    def download(self, uidb64):
         try:
             faculty_member = force_text(urlsafe_base64_decode(uidb64))
-            #user = User.objects.get(pk=uid)
-        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        except: #(TypeError, ValueError, OverflowError):
             faculty_member = None
 
         if faculty_member is not None:
@@ -38,40 +111,101 @@ class FacExtraCreditFiles(View):
                     if faculty_member in fname:
                         zip.write(fpath+fname, fname)
 
-                # fix for Linux zip files read in Windows
-                for file in zip.filelist:
-                    file.create_system = 0
+            # fix for Linux zip files read in Windows
+            for file in zip.filelist:
+                file.create_system = 0
 
-                zip.close()
+            zip.close()
+            
+            response = HttpResponse(content_type="application/zip")
+            response['Content-Disposition'] = 'attachment; filename=' + faculty_member + '_ec_files.zip'
 
-                response = HttpResponse(content_type='application/zip')
-                response['Content-Disposition'] = 'attachment; filename=' + faculty_member + '_ec_files.zip'
+            in_memory.seek(0)
+            response.write(in_memory.read())
 
-                in_memory.seek(0)
-                response.write(in_memory.read())
-
-                return response
+            return response
         else:
             return HttpResponse('Unable to serve extra credit files. Please try again later or contact the ACM team.')
 
 
+class GenerateDomJudgeTSV(View):
+
+    def get(self, request):
+        tasks.generate_contest_files.delay()
+        messages.info(request, 'Generate Contest TSVs task scheduled. Refresh page in a few seconds use download link.', fail_silently=True)
+
+        return redirect('admin_dashboard')
+
+
+class GenerateExtraCreditReports(View):
+
+    def get(self, request):
+        tasks.generate_ec_reports.delay()
+        messages.info(request, 'Generate extra credit reports task scheduled. Refresh page in a few seconds use download and email links.', fail_silently=True)
+
+        return redirect('admin_dashboard')
+
+
 @staff_member_required
+@transaction.atomic
 def dashboard(request):
     context = {}
 
     if request.method == 'POST':
         walkin_form = forms.GenerateWalkinForm(request.POST)
-
+        file_form = forms.ResultsForm(request.POST, request.FILES)
         if walkin_form.is_valid():
-            if walkin_form.cleaned_data['total'] and walkin_form.cleaned_data['division']:
-                tasks.create_walkin_teams.delay(int(walkin_form.cleaned_data['division']), int(walkin_form.cleaned_data['total']))
-                
-                messages.info(request, 'Create teams task scheduled.', fail_silently=True)
-                return redirect('admin_dashboard')
+            tasks.create_walkin_teams.delay(int(walkin_form.cleaned_data['division']), int(walkin_form.cleaned_data['total']))  
+            messages.info(request, 'Create teams task scheduled.', fail_silently=True)
+        elif file_form.is_valid():
+            if Contest.objects.all().count() == 0:
+                file_form.save()
+                tasks.process_contest_results.delay()
+                messages.success(
+                        request, 'Results uploaded.', fail_silently=True)
+            else:
+                try:
+                    contest = Contest.objects.all().first()
+                except:
+                    messages.error(
+                        request, 'Failed to upload results. Please try again.', fail_silently=True)
+                else:
+                    messages.success(
+                        request, str(file_form.cleaned_data['results']), fail_silently=True)
+                    contest.results = request.FILES['results']
+                    contest.save()
+                    tasks.process_contest_results.delay()
+                    messages.success(
+                        request, 'Results uploaded.', fail_silently=True)
 
+        return redirect('admin_dashboard')
     else:
-        forms.GenerateWalkinForm()
+        walkin_form = forms.GenerateWalkinForm()
+        file_form = forms.ResultsForm()
+        
+    '''try:
+        contest = Contest.objects.all().first().get()
+    except:
+        context['ec_available'] = False
+    else:
+        context['ec_available'] = contest.ec_processed'''
 
+    if Team.objects.exclude(questions_answered=0).count() > 0:
+        context['dj_results_processed'] = True
+    else:
+        context['dj_results_processed'] = False
+
+    if len(os.listdir(MEDIA_ROOT + '/ec_files/')) > 0:
+        context['ec_files_available'] = True
+    else:
+        context['ec_files_available'] = False
+
+    if len(os.listdir(MEDIA_ROOT + '/contest_files/')) > 0:
+        context['dj_files_available'] = True
+    else:
+        context['dj_files_available'] = False
+    
+    
     context['users_registered'] = User.objects.all().count()
     context['users_verified'] = User.objects.filter(is_active=True).count()
     context['added_fsu_num'] = Profile.objects.exclude(fsu_num=None).count()
@@ -91,30 +225,7 @@ def dashboard(request):
     context['num_lower_reg_participants'] = Profile.objects.filter(team__division=2).exclude(team__name__contains='Walk-in-').count()
     context['num_lower_walkin_participants'] = Profile.objects.filter(team__division=2).filter(team__name__contains='Walk-in-').count()
 
-    context['gen_walkin_form'] = forms.GenerateWalkinForm()
+    context['file_form'] = file_form
+    context['gen_walkin_form'] = walkin_form
     context['courses'] = Course.objects.all()
     return render(request, 'contestadmin/dashboard.html', context)
-
-
-@staff_member_required
-def download_ec_files(request):
-    in_memory = BytesIO()
-    zip = ZipFile(in_memory, "a")
-    
-    fpath = MEDIA_ROOT + '/ec_files/'
-    for fname in os.listdir(fpath):
-        zip.write(fpath+fname, fname)
-
-    # fix for Linux zip files read in Windows
-    for file in zip.filelist:
-        file.create_system = 0 
-
-    zip.close()
-
-    response = HttpResponse(content_type="application/zip")
-    response["Content-Disposition"] = "attachment; filename=all_ec_files.zip"
-    
-    in_memory.seek(0)    
-    response.write(in_memory.read())
-    
-    return response
