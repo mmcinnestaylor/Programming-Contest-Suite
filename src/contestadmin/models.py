7ee1447dc5925d7a5109b09022f3b21e77ff2bcd
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2021-09-26 23:20:32 -0500

    updated contest model and project migrations

--- src/contestadmin/models.py
+++ src/contestadmin/models.py
@@ -11,7 +11,12 @@ class Contest(models.Model):
     - Results field for DOMJudge contest results uploading
     """
 
-    contest_date = models.DateTimeField(auto_now=False)
+    contest_date = models.DateField(auto_now=False)
+    contest_doors = models.TimeField(auto_now=False)
+    contest_start = models.TimeField(auto_now=False)
+    contest_freeze = models.TimeField(auto_now=False)
+    contest_end = models.TimeField(auto_now=False)
+    contest_awards = models.TimeField(auto_now=False)
     results = models.FileField(upload_to='uploads/', blank=True)
     ec_processed = models.BooleanField(default=False)
         
