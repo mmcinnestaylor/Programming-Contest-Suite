1f92a6b2373b1495a6385d8f464ef4aa6f54a9da
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2021-10-13 00:22:03 -0500

    updated credentials

--- src/core/management/commands/initadmin.py
+++ src/core/management/commands/initadmin.py
@@ -5,7 +5,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if User.objects.count() == 0:
-            admin = User.objects.create_superuser(username='dev', email='webmaster@fsu.acm.org', password='seminoles1!')
+            admin = User.objects.create_superuser(username='contestadmin', email='contest@fsu.acm.org', password='seminoles1!')
             admin.save()
         else:
             print('Admin accounts can only be initialized if no Accounts exist')
