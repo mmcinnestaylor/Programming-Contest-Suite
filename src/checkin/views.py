cb8f82fbde983fae51539c482546f8adb9367cc0
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2021-12-18 11:39:53 -0600

    corrected walk-in check in
    -- walk-in contestants can view team pin, but cannot admin

--- src/checkin/views.py
+++ src/checkin/views.py
@@ -24,90 +24,101 @@ def checkin(request):
 		is_walkin = False
 
 		if email_form.is_valid() and swipe_form.is_valid() and walkin_form.is_valid():
+			# Process a walk-in selection, if one is made.
 			if walkin_form.cleaned_data['division']:
-				try:
-					if walkin_form.cleaned_data['division'] == 1:
-						walk_in_team = Team.objects.filter(name__contains='Walk-in-U-').filter(num_members=0).first()
-					else:
-						walk_in_team = Team.objects.filter(name__contains='Walk-in-L-').filter(num_members=0).first()
-				except:
-					messages.error(request, 'Walk-in team assignment failed. Please reattempt check-in.', fail_silently=True)
+				# Get team from selected division
+				if walkin_form.cleaned_data['division'] == '1':
+					walk_in_team = Team.objects.filter(name__contains='Walk-in-U-').filter(num_members=0).first()						
+				else:
+					walk_in_team = Team.objects.filter(name__contains='Walk-in-L-').filter(num_members=0).first()
+				
+				# If an empty walkin-is is not found, notify user.
+				if walk_in_team is None:
+					messages.error(request, 'There are no Walk-in teams available.', fail_silently=True)
 					return redirect('checkin_result')
-				else:
-					is_walkin = True
-					if walk_in_team is None:
-						messages.error(request, 'There are no Walk-in teams available.', fail_silently=True)
-						return redirect('checkin_result')
-			if email_form.cleaned_data['email']:
-				try:
-					user = User.objects.get(email=email_form.cleaned_data['email'])
-				except:
-					messages.error(
-						request, 'Checkin failed. Email address not found.', fail_silently=True)
-				else:
-					if user.profile.checked_in:
-						messages.info(request, 'You are already checked in.', fail_silently=True)
-					elif user.profile.team is None:
-						messages.info(request, 'You are not a member of a registered team. Join a registered team, or select YES at the walk-in prompt.', fail_silently=True)
-					else:
-						user.profile.checked_in = True
-						if is_walkin == True:
-							if user.profile.has_team:
-								messages.info(request, 'You are a member of a registered team. Walk-in selection ignored.', fail_silently=True)
-							else:
-								# update user
-								user.profile.team = walk_in_team
-								user.profile.team_admin = True
+				
+				is_walkin = True
 
-								#update team
-								walk_in_team.num_members += 1
-								walk_in_team.members.append(user.get_full_name())
-								walk_in_team.save()
-
-						user.save()
-
-						tasks.send_credentials.delay(user.username)
-						messages.success(request, str(user.first_name) + ', you are checked in!', fail_silently=True)
-						messages.info(request, 'Check your registered email or account dashboard for DOMJudge credentials.', fail_silently=True)
-			elif swipe_form.cleaned_data['fsu_num']:
+			# Process email form entry, if it has been made.
+			if swipe_form.cleaned_data['fsu_num']:
 				if swipe_form.valid_read():
-					fsu_num = swipe_form.parse()
-					
+					fsu_number = swipe_form.parse()
+
 					try:
-						user = User.objects.get(profile__fsu_num=fsu_num)
+						user = User.objects.get(profile__fsu_num=fsu_number)
 					except:
-						messages.error(request, 'Checkin failed. FSU number not found.', fail_silently=True)
+						messages.error(
+							request, 'Check in failed. FSU number not found.', fail_silently=True)
 					else:
 						if user.profile.checked_in:
-							messages.info(request, 'You are already checked in.', fail_silently=True)
-						elif user.profile.team is None:
-							messages.info(request, 'You are not a member of a registered team. Join a registered team, or select YES at the walk-in prompt.', fail_silently=True)
+							messages.info(request, 'You are already checked in.',
+							              fail_silently=True)
+						elif user.profile.team is None and not is_walkin:
+							messages.info(
+								request, 'You are not a member of a registered team. Join a registered team, or select YES at the walk-in prompt.', fail_silently=True)
 						else:
 							user.profile.checked_in = True
 							if is_walkin == True:
-								if user.profile.has_team:
-									messages.info(request, 'You are a member of a registered team. Walk-in selection ignored.', fail_silently=True)
+								if user.profile.has_team():
+									messages.info(
+										request, 'You are a member of a registered team. Walk-in selection ignored.', fail_silently=True)
 								else:
 									# update user
 									user.profile.team = walk_in_team
-									user.profile.team_admin = True
 
 									#update team
 									walk_in_team.num_members += 1
-									walk_in_team.members.append(user.get_full_name())
 									walk_in_team.save()
 
 							user.save()
 
+							# Email user DOMjudge credentials
 							tasks.send_credentials.delay(user.username)
-							messages.success(request, str(user.first_name) + ', you are checked in!', fail_silently=True)
-							messages.info(request, 'Check your registered email or account dashboard for DOMJudge credentials.', fail_silently=True)
+							messages.success(request, str(user.first_name) +
+							                 ', you are checked in!', fail_silently=True)
+							messages.info(
+								request, 'Check your registered email or account dashboard for DOMJudge credentials.', fail_silently=True)
 				else:
-					messages.error(request, 'Invalid card read. Please try again.', fail_silently=True)
+					messages.error(
+						request, 'Invalid card read. Please try again.', fail_silently=True)
+
+			# Process email form entry, if it has been made.
+			elif email_form.cleaned_data['email']:
+				try:
+					user = User.objects.get(email=email_form.cleaned_data['email'])
+				except:
+					messages.error(
+						request, 'Check in failed. Email address not found.', fail_silently=True)
+				else:
+					if user.profile.checked_in:
+						messages.info(request, 'You are already checked in.', fail_silently=True)
+					elif user.profile.team is None and not is_walkin:
+						messages.info(request, 'You are not a member of a registered team. Join a registered team, or select YES at the walk-in prompt.', fail_silently=True)
+					else:
+						user.profile.checked_in = True
+						if is_walkin == True:
+							if user.profile.has_team():
+								messages.info(request, 'You are a member of a registered team. Walk-in selection ignored.', fail_silently=True)
+							else:
+								# update user
+								user.profile.team = walk_in_team
+
+								#update team
+								walk_in_team.num_members += 1
+								walk_in_team.save()
+
+						user.save()
+
+						# Email user DOMjudge credentials
+						tasks.send_credentials.delay(user.username)
+						messages.success(request, str(user.first_name) + ', you are checked in!', fail_silently=True)
+						messages.info(request, 'Check your registered email or account dashboard for DOMJudge credentials.', fail_silently=True)
+			
 			else:
 				messages.error(request, 'Invalid form submission. Please resubmit your information.', fail_silently=True)
 
 			return redirect('checkin_result')
+
 	else:
 		email_form = forms.EmailCheckinForm()
 		swipe_form = forms.SwipeCheckinForm()
