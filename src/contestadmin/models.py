e4a76adf1eb8fa7b6b0298d6c6880792df638e0c
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2022-03-21 17:09:03 -0500

    added participation type to contest details

--- src/contestadmin/models.py
+++ src/contestadmin/models.py
@@ -9,6 +9,11 @@ class Contest(models.Model):
     Contest Model
     - Results field for DOMJudge contest results uploading
     """
+    FORMAT = (
+        (1, 'In-Person'),
+        (2, 'Virtual'),
+        (3, 'Hybrid')
+    )
 
     contest_date = models.DateField(auto_now=False)
     contest_doors = models.TimeField(auto_now=False, blank=True, null=True)
@@ -19,7 +24,8 @@ class Contest(models.Model):
     team_deadline = models.DateTimeField(auto_now=False, blank=True, null=True)
     results = models.FileField(upload_to='uploads/', blank=True)
     ec_processed = models.BooleanField(default=False)
-    volunteer_pin = models.CharField(max_length=8, default=User.objects.make_random_password(length=8))
+    volunteer_pin = models.CharField(max_length=8, default='thankyou')
+    participation = models.PositiveSmallIntegerField(choices=FORMAT, blank=True, null=True)
         
     def __str__(self):
         return ("Programming Contest "+str(self.contest_date))
