e4de762fe196b2bbea0aa04776aa2cc9ebd54671
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2022-04-11 14:05:29 -0500

    added contest announcements delivered to discord webhook

--- src/announcements/signals.py
+++ src/announcements/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from .models import Announcement
-from .tasks import email_annoucement
+from .tasks import email_annoucement, discord_announcement
 
 
 @receiver(post_save, sender=Announcement)
@@ -10,6 +10,7 @@ def send_announcement(sender, instance, created, **kwargs):
 	# Publish
 	if instance.status == 1:
 		email_annoucement.delay(instance.id)
+		discord_announcement.delay(instance.id)
 	# Draft
 	else:
 		pass
--- src/announcements/tasks.py
+++ src/announcements/tasks.py
@@ -5,8 +5,10 @@ from django.template.loader import render_to_string
 from celery import shared_task
 from celery.utils.log import get_task_logger
 
+from discord import Webhook, RequestsWebhookAdapter, Embed, InvalidArgument
+
 from .models import Announcement
-from contestsuite.settings import DEFAULT_FROM_EMAIL
+from contestsuite.settings import ANNOUNCEMENT_WEBHOOK_URL, DEFAULT_FROM_EMAIL, ALLOWED_HOSTS
 
 
 logger = get_task_logger(__name__)
@@ -35,3 +37,30 @@ def email_annoucement(id):
         send_mass_mail(messages, fail_silently=False)
 
         logger.info('Sent announcement to %d users' % i)
+
+@shared_task
+def discord_announcement(id):
+    try:
+        announcement = Announcement.objects.get(id=id)
+    except:
+        logger.info('Failed to send announcement with id %d ' % id)
+    else:
+        # Initializing webhook
+        try:
+            webhook = Webhook.from_url(
+                ANNOUNCEMENT_WEBHOOK_URL, adapter=RequestsWebhookAdapter())
+        except InvalidArgument:
+            logger.info('Failed to connect to announcement webhook')
+        else:
+            url = 'https://'+ALLOWED_HOSTS[0]+announcement.get_absolute_url()
+            
+            if len(announcement.content) <= 140:
+                content = announcement.content
+            else:
+                content = announcement.content[:140] + '...'
+            
+            # Initializing an Embed
+            embed = Embed(title=announcement.title, description=content, url=url)
+
+            # Executing webhook.
+            webhook.send(embed=embed)
