c05fd19b958f86b0d25903da9157577ca9d80454
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2022-04-19 23:34:39 -0500

    added url for username recovery view

--- src/register/urls.py
+++ src/register/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.base, name='register_base'),
     path('account/', views.account, name='register_account'),
     path('activate/<uidb64>/<token>/', views.ActivateAccount.as_view(), name='activate'),
+    path('recover_username/', views.recover_username, name='recover_username'),
     path('team/', views.team, name='register_team'),
 ]
