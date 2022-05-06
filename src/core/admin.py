dfb95d870ce20cbdaf3b9bad23ad1a9b24590d8e
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2022-05-05 21:33:42 -0500

    added user table export

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+
+class UserResource(resources.ModelResource):
+	class Meta:
+		model = User
+		fields = ('first_name', 'last_name', 'email', 'is_active', 'profile__checked_in')
+
+
+class UserAdmin(ImportExportModelAdmin):
+    resource_class = UserResource
+
 
 # Register your models here.
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
