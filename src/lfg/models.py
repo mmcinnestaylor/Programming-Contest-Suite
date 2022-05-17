0342c5974b66d6d38f324e1984ac382fb923ddcd
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2022-05-16 19:05:37 -0500

    created model to store discord ids

--- src/lfg/models.py
+++ src/lfg/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class DiscordMember(models.Model):
+    username = models.CharField(max_length=32)
+    discriminator = models.SmallIntegerField()
+
+    def __str__(self):
+        return (str(self.username)+'#'+str(self.discriminator).zfill(4))
