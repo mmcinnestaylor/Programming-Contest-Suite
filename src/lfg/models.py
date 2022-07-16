d11fc3c13efcfebbfa4b75d4bd9812927621c1f5
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2022-07-15 22:40:11 -0500

    created lfg profile model

--- src/lfg/models.py
+++ src/lfg/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -9,3 +10,37 @@ class DiscordMember(models.Model):
 
     def __str__(self):
         return (str(self.username)+'#'+str(self.discriminator).zfill(4))
+
+
+class LFGProfile(models.Model):
+    """
+    LFG Profile Model
+    - Class for looking for group feature
+    """
+
+    DIVISION = (
+        (1, 'Upper Division'),
+        (2, 'Lower Division')
+    )
+
+    STANDING = (
+        (1, 'Graduate'),
+        (2, 'Senior'),
+        (3, 'Junior'),
+        (4, 'Sophomore'),
+        (5, 'Freshman'),
+        (6, 'Other'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    discord_username = models.CharField(max_length=32)
+    discord_discriminator = models.SmallIntegerField()
+    division = models.PositiveSmallIntegerField(
+        choices=DIVISION, blank=True, null=True)
+    standing = models.PositiveSmallIntegerField(
+        choices=STANDING, blank=True, null=True)
+    active = models.BooleanField(default=False)
+    verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return (str(self.user.first_name) + ' ' + str(self.user.last_name)+' - '+str(self.discord_username)+'#'+str(self.discord_discriminator).zfill(4))
