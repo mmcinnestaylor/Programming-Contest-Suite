568c423a13aa621e81931c2baccc1e34d5212707
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2022-09-05 17:33:19 -0500

    added additional state field to lfg profile model + discriminator validators
    - implemented helper functions to retrieve username and check profile completion status

--- src/lfg/models.py
+++ src/lfg/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 # Create your models here.
 
@@ -34,13 +34,20 @@ class LFGProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     discord_username = models.CharField(max_length=32)
-    discord_discriminator = models.SmallIntegerField()
+    discord_discriminator = models.SmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(9999)])
     division = models.PositiveSmallIntegerField(
         choices=DIVISION, blank=True, null=True)
     standing = models.PositiveSmallIntegerField(
         choices=STANDING, blank=True, null=True)
     active = models.BooleanField(default=False)
+    completed = models.BooleanField(default=False)
     verified = models.BooleanField(default=False)
 
     def __str__(self):
         return (str(self.user.first_name) + ' ' + str(self.user.last_name)+' - '+str(self.discord_username)+'#'+str(self.discord_discriminator).zfill(4))
+
+    def get_discord_username(self):
+        return str(self.discord_username)+'#'+str(self.discord_discriminator).zfill(4)
+
+    def is_completed(self):
+        return self.division is not None and self.standing is not None
