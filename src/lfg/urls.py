2ac50150cb9943429e06c47d61790589e4631bcf
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2022-09-12 10:53:40 -0500

    url initial implementation

--- src/lfg/urls.py
+++ src/lfg/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     # contest suite homepage
     path('', views.dashboard, name='lfg_dashboard'),
+    path('profile/activate/', views.activate_profile, name='activate_lfg_profile'),
+    path('profile/create/', views.create_profile, name='create_lfg_profile'),
+    path('profile/deactivate/', views.deactivate_profile, name='deactivate_lfg_profile'),
+    path('profile/manage/', views.manage_profile, name='manage_lfg_profile'),
 ]
