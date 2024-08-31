f44faf4a379e2fefda55ee71ff486f4f7803da50
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2024-08-30 21:48:18 -0500

    Updated default superuser creation
    - Updated user name to include last name
    - Clarified system message when user is not created

--- src/core/management/commands/initadmin.py
+++ src/core/management/commands/initadmin.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if User.objects.count() == 0:
-            admin = User.objects.create_superuser(username='contestadmin', email='contest@fsu.acm.org', password='seminoles1!', first_name='Administrator')
+            admin = User.objects.create_superuser(username='contestadmin', email='contest@fsu.acm.org', password='seminoles1!', first_name='Contest Admin', last_name='Team')
             admin.save()
         else:
-            print('Admin accounts can only be initialized if no Accounts exist')
+            print('Skipping default superuser account creation. The account already exists.')
