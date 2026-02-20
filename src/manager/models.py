41f5d02362d148d15a180a8d11b277a7d449a983
ramonicv <pepsidog0@proton.me>
2026-02-20 17:17:06 -0500

    added passed_cop3330 boolean field to Profile model

--- src/manager/models.py
+++ src/manager/models.py
@@ -114,6 +114,7 @@ class Profile(models.Model):
     checked_in = models.BooleanField(default=False)
     email_confirmed = models.BooleanField(default=False)
     announcement_email_opt_out = models.BooleanField(default=False)
+    passed_cop3330 = models.BooleanField(default=True)
     
     def __str__(self):
         return (str(self.user.first_name) + ' ' + str(self.user.last_name))
